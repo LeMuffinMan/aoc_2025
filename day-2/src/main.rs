1b6d77d99097fc7d157fc56d22b7ab89dd54fc05
Olivier Elleaume <oelleaum@z1r6p2.42lyon.fr>
2025-12-02 19:26:02 +0100

    day2 part1

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -6,39 +6,39 @@ use std::error::Error;
 fn find_invalids_ids(range: &str, count: &mut u64) -> u64 {
     if let Some((low, high)) = range.split_once('-') {
         println!("splitted = {low} {high}");
-        let mut num_low: u64 = low.parse().unwrap();
-        let mut num_high: u64 = high.parse().unwrap();
+        let num_low: u64 = low.parse().unwrap();
+        let num_high: u64 = high.parse().unwrap();
         // if num_low > num_high {
         //     let mut tmp = num_low;
         //     num_low = num_high;
         //     num_high = tmp;
         // }
-        for ref mut n in num_low..=num_high {
+        for n in num_low..=num_high {
             let n_str = n.to_string();
             // print!("n_str = {n_str} : ");
             if n_str.len() % 2 == 0 {
                 let (left, right) = n_str.split_at(n_str.len() / 2);  
                 if left == right {
                     println!("INVALID : {n}");
-                    *count += *n;
+                    *count += n;
                 } else {
                     println!("Valid : {n}");
                 }
             } else {
                 println!("Valid : {n}");
             }
-            *n += 1;
         }
-    }
+    } 
     *count
 }
 //19562842130 -> too low
 //19562842152 -> too low
-//
+//19574776074
 //20045852680 -> going down -> too high
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = get_input()?;
+    // println!("{:?}", input);
     let mut count = 0;
     // let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
     let sequences = input.split(','); 
@@ -63,7 +63,8 @@ fn get_input() -> Result<String, Box<dyn Error>> {
         .send()?
         .text()?;
 
-    let line = response[0..response.len() - 2].to_string();
+    // let line = response[0..response.len() - 2].to_string();
+    let line = response.trim().to_string();
     Ok(line)
 }
 
