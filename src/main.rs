c03dce788b6fc372eeef4aeed708ba7ce059c642
muffin <muffin@arch.man>
2025-12-05 11:51:36 +0100

    part 1 started

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use crate::input::get_input;
+use crate::day_5::solve_day_5;
 use crate::day_4::solve_day_4;
 use crate::day_3::solve_day_3;
 use crate::day_2::solve_day_2;
 use crate::day_1::solve_day_1;
 mod input;
+mod day_5;
 mod day_4;
 mod day_3;
 mod day_2;
@@ -17,6 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "2" => solve_day_2(input),
         "3" => solve_day_3(input),
         "4" => solve_day_4(input),
+        "5" => solve_day_5(input),
         _ => {
             println!("Day {} not implemented", day);
         }
