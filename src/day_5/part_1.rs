0f79525c2356c546a9559f7a84aa6d919fa51b0c
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-12-05 22:12:25 +0100

    day5 part1

--- src/day_5/part_1.rs
+++ src/day_5/part_1.rs
@@ -13,51 +13,27 @@ fn split_input(input : &mut Vec<String>) -> (Vec<String> , Vec<String>) {
   (ranges, ingredients)
 }
 
-fn count_fresh_ingredients(ranges: &Vec<String>, ing: String) -> bool {
+fn is_fresh_ingredients(ranges: &Vec<String>, ing: &String) -> bool {
   for range in ranges {
-    let Some((low, high)) = range.split_once('-') else { println!("range [{range}] not splitable on -"); return false };
-    let ing_len = ing.len();
-    let low_len = low.len();
-    let high_len = high.len();
-    println!("ing = {ing} | low = {low} | high = {high}");
-    if ing_len < low_len || ing_len < high_len {
-      return false;
+    let Some((low, high)) = range.split_once('-') else { unreachable!() };
+    if ing.parse::<u64>().unwrap() >= low.parse::<u64>().unwrap() && ing.parse::<u64>().unwrap() <= high.parse::<u64>().unwrap() {
+        return true;
     }
-    if ing_len == low_len {
-        let ing = ing.as_ptr();
-        let low = low.as_ptr();
-    }
-    else if ing_len == high_len {
-    }
-    println!("unreachable");
   }
-  false
+  return false;
 }
 
 pub fn part_1(mut input: &mut Vec<String>) -> u64 {
-  let mut input = vec![
-      "3-5".to_string(),
-      "10-14".to_string(),
-      "16-20".to_string(),
-      "12-18".to_string(),
-      "\n".to_string(),
-      "1".to_string(),
-      "5".to_string(),
-      "8".to_string(),
-      "11".to_string(),
-      "17".to_string(),
-      "32".to_string(),
-  ];
   let mut count = 0;
   let (ranges, ingredients) = split_input(&mut input);
   for range in &ranges {
-    // println!("Range = {range}");
+    println!("Range = {range}");
   }
   for ing in ingredients {
-    // println!("Ing = {ing}");
-    if !count_fresh_ingredients(&ranges, ing) {
+    println!("Ing = {ing}");
+    if is_fresh_ingredients(&ranges, &ing) {
       count += 1;
-    };
+    }
   }
   println!("count = {count}");
   count as u64
