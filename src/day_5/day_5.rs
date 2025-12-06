9fe89399cada93dfed3b77ae4c195a348d735e5d
muffin <muffin@arch.man>
2025-12-06 02:51:58 +0100

    day3 and day5 solved

--- src/day_5/day_5.rs
+++ src/day_5/day_5.rs
@@ -4,7 +4,7 @@ use crate::day_5::part_1::part_1;
 use crate::day_5::part_2::part_2;
 
 pub fn solve_day_5(input: Vec<String>)  {
-    // println!("Part 1 password = {:?}", part_1(&mut input.clone()));
+    println!("Part 1 password = {:?}", part_1(&mut input.clone()));
     println!("Part 2 password = {:?}", part_2(&input));
 }
   
--- src/day_5/part_1.rs
+++ src/day_5/part_1.rs
@@ -26,15 +26,15 @@ fn is_fresh_ingredients(ranges: &Vec<String>, ing: &String) -> bool {
 pub fn part_1(mut input: &mut Vec<String>) -> u64 {
   let mut count = 0;
   let (ranges, ingredients) = split_input(&mut input);
-  for range in &ranges {
-    println!("Range = {range}");
-  }
+  // for range in &ranges {
+  //   println!("Range = {range}");
+  // }
   for ing in ingredients {
-    println!("Ing = {ing}");
+    // println!("Ing = {ing}");
     if is_fresh_ingredients(&ranges, &ing) {
       count += 1;
     }
   }
-  println!("count = {count}");
+  // println!("count = {count}");
   count as u64
 }
