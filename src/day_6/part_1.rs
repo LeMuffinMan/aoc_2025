dd0314324893520c9625e6d745cc3c79261a4d92
muffin <muffin@arch.man>
2025-12-06 12:13:40 +0100

    without comments

--- src/day_6/part_1.rs
+++ src/day_6/part_1.rs
@@ -5,17 +5,14 @@ pub fn part_1(input: &Vec<String>) -> u64 {
   let mut worksheet : Vec<Vec<u64>> = Vec::new(); 
   let mut ops : Vec<char> = Vec::new();
   for line in input {
-    println!("line = {line}");
     if line.find('+').is_some() || line.find('*').is_some() {
       ops.extend(line.chars().filter(|&c| c == '+' || c == '*'));
       break;
     }
     worksheet.push(line.split_whitespace().map(|v| v.parse::<u64>().unwrap()).collect());
-    println!("worksheet[{count}] = {:?}", worksheet[count]);
     count += 1;
   }
   count = 0;
-  println!("Operation= {:?}", ops);
   for (i, op) in ops.iter().enumerate() {
     let mut numbers = Vec::new();
     for row in &worksheet {
@@ -26,7 +23,6 @@ pub fn part_1(input: &Vec<String>) -> u64 {
       '*' => numbers.iter().product::<u64>(), 
       _ => unreachable!(),
     };
-    println!("res = {res}");
     count += res as usize;
   }
   count as u64
