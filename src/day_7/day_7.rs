47f12919940391432eed53f80e618f51ac19dca6
muffin <muffin@arch.man>
2025-12-07 12:55:26 +0100

    day 7 part 2

--- src/day_7/day_7.rs
+++ src/day_7/day_7.rs
@@ -5,6 +5,6 @@ use crate::day_7::part_2::part_2;
 
 pub fn solve_day_7(input: Vec<String>)  {
     println!("Part 1 password = {:?}", part_1(&input));
-    // println!("Part 2 password = {:?}", part_2(&input));
+    println!("Part 2 password = {:?}", part_2(&input));
 }
   
