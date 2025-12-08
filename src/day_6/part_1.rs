ed337d95e7ae59db1aeac5a8bfe9dcb8e3960f6f
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-12-08 03:40:40 +0100

    refacto part1

--- src/day_6/part_1.rs
+++ src/day_6/part_1.rs
@@ -1,6 +1,6 @@
-pub fn part_1(input: &Vec<String>) -> u64 {
-    let mut worksheet: Vec<Vec<u64>> = Vec::new();
+fn get_worksheet(input: &Vec<String>) -> (Vec<Vec<u64>>, Vec<char>) {
     let mut ops: Vec<char> = Vec::new();
+    let mut worksheet: Vec<Vec<u64>> = Vec::new();
     for line in input {
         if line.find('+').is_some() || line.find('*').is_some() {
             ops.extend(line.chars().filter(|&c| c == '+' || c == '*'));
@@ -12,18 +12,26 @@ pub fn part_1(input: &Vec<String>) -> u64 {
                 .collect(),
         );
     }
-    let mut count = 0;
+    (worksheet, ops)
+}
+
+fn solve_worksheet(worksheet: Vec<Vec<u64>>, ops: Vec<char>) -> u64 {
+    let mut res = 0;
     for (i, op) in ops.iter().enumerate() {
         let mut numbers = Vec::new();
         for row in &worksheet {
             numbers.push(row[i]);
         }
-        let res = match op {
+        res += match op {
             '+' => numbers.iter().sum(),
             '*' => numbers.iter().product::<u64>(),
             _ => unreachable!(),
         };
-        count += res as usize;
     }
-    count as u64
+    res
+}
+
+pub fn part_1(input: &Vec<String>) -> u64 {
+    let (worksheet, ops) = get_worksheet(&input);
+    solve_worksheet(worksheet, ops)
 }
