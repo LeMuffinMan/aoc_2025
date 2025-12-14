33dafef045bf4a9736f8f59a6874d57e29de79b8
Olivier Elleaume <oelleaum@z4r2p3.42lyon.fr>
2025-12-14 17:25:37 +0100

    minor fix on aoc.sh and attempt opti day8

--- src/day_10/day_10.rs
+++ src/day_10/day_10.rs
@@ -1,10 +1,11 @@
 
+#[allow(unused_imports)]
 use crate::day_10::part_1::part_1;
 #[allow(unused_imports)]
 use crate::day_10::part_2::part_2;
 
 pub fn solve_day_10(input: Vec<String>)  {
-    println!("Part 1 password = {:?}", part_1(&input));
+    // println!("Part 1 password = {:?}", part_1(&input));
     // println!("Part 2 password = {:?}", part_2(&input));
 }
   
--- src/day_8/mod.rs
+++ src/day_8/mod.rs
@@ -1,5 +1,6 @@
 pub mod day_8;
 pub mod junction_boxe;
+pub mod circuit;
 pub mod part_1;
 pub mod part_2;
 
